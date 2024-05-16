75aa7d18d6f1ffbb84b883346715544d8c3cca4f
ooocso <161703408+ooocso@users.noreply.github.com>
2024-05-16 18:04:29 +0800

    0516

---
+++ 0516/ListViewDemo/app/src/main/java/com/example/listviewdemo/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.listviewdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private ListView lv;
+    private  String[] cities;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cities = getResources().getStringArray(R.array.cities);
+        lv = (ListView) findViewById(R.id.listview);
+
+        ArrayAdapter<String> adpcities = new ArrayAdapter<>(this, android.R.layout.simple_list_item_single_choice,cities);
+       lv.setAdapter(adpcities);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                TextView output = (TextView) findViewById(R.id.lblOutput);
+
+                output.setText("你是住在"+ cities[position]);
+                Toast.makeText(MainActivity.this,"你是住在: "+cities[position], Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
